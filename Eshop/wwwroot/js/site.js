b802685a53ba002a0a5fc402e2c264b3c6ccdb4c
Grzesiek <grze.mitu@gmail.com>
2022-06-14 17:46:03 +0200

    Move js scripts to site.js file
    Also fixed wrong link destination in Owned.cshtml

--- Eshop/wwwroot/js/site.js
+++ Eshop/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const rows = document.querySelectorAll("div[data-href]");
+
+    rows.forEach(row => {
+        row.addEventListener("click", () => {
+            window.location.href = row.dataset.href;
+        })
+    })
+})
